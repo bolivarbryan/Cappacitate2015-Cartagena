01f592470cfb9fe9f9b3cd1fa67b4425213c12fc
Bryan A Bolivar M <boli@Mac-mini-de-Bryan.local>
2015-05-08 14:50:14 -0500

    user model

---
+++ Formulario/Formulario/Model/Usuario.h
@@ -0,0 +1,14 @@
+//
+//  Usuario.h
+//  Formulario
+//
+//  Created by Bryan A Bolivar M on 5/8/15.
+//  Copyright (c) 2015 Bolivar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Usuario : NSObject
+ @property (nonatomic, assign) NSString * username;
+ @property (nonatomic, assign) NSString * password;
+@end
---
+++ Formulario/Formulario/Model/Usuario.m
@@ -0,0 +1,13 @@
+//
+//  Usuario.m
+//  Formulario
+//
+//  Created by Bryan A Bolivar M on 5/8/15.
+//  Copyright (c) 2015 Bolivar. All rights reserved.
+//
+
+#import "Usuario.h"
+
+@implementation Usuario
+
+@end
